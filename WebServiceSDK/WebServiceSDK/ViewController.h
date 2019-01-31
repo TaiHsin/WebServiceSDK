f8de372335b182a6176027c8ca3284751fd1b5ce
taihsinlee <taihsinlee@kkbox.com>
2019-01-31 18:09:50 +0800

    Create WebServiceSDK project

---
+++ WebServiceSDK/WebServiceSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebServiceSDK
+//
+//  Created by Tsihsin Lee on 2019/1/31.
+//  Copyright © 2019 Tsihsin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
