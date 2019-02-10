06d2271cde52e06cf288412c3df5eea6016ada08
TaiHsin <peterlee0466@gmail.com>
2019-02-10 14:24:56 +0800

    Applied fetchIamge function

--- WebServiceSDK/WebServiceSDK/HTTPClient.m
+++ WebServiceSDK/WebServiceSDK/HTTPClient.m
@@ -14,29 +14,66 @@
 - (void) fetchGetResponseWithCallback: (void(^)(NSDictionary *, NSError *))callback {
     NSString * dataUrl = @"https://httpbin.org/get";
     NSURL * url = [NSURL URLWithString: dataUrl];
+    NSURLRequest * request = [NSURLRequest requestWithURL: url];
+    NSURLSession * session = [NSURLSession sharedSession];
     
-    NSURLSessionDataTask * downloadTask = [[NSURLSession sharedSession] dataTaskWithURL: url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-        if (data != NULL) {
-            NSError * parsError;
-            NSDictionary * dict = [NSJSONSerialization JSONObjectWithData: data options: 0 error: &parsError];
-            callback(dict, parsError);
-            
-        } else {
-            
-            callback(nil, error);
-        }
-    }];
+    NSURLSessionDataTask * dataTask =
+    [session dataTaskWithRequest: request
+               completionHandler:^(NSData * _Nullable data,
+                                   NSURLResponse * _Nullable response,
+                                   NSError * _Nullable error) {
+                   
+                   dispatch_async(dispatch_get_main_queue(), ^{
+                       
+                       if (data != NULL) {
+                           NSError * parseError;
+                           NSDictionary * dict = [NSJSONSerialization JSONObjectWithData: data
+                                                                                 options: 0
+                                                                                   error: &parseError
+                                                  ];
+                           callback(dict, parseError);
+                           
+                       } else {
+                           
+                           callback(nil, error);
+                       }
+                   });
+               }
+     ];
     
-    [downloadTask resume];
+    [dataTask resume];
 }
 
-- (void) postCustomerName: (NSString *)name callback:(void(^)(NSDictionary *, NSError *))callback {
+- (void) postCustomerName: (NSString *)name callback: (void(^)(NSDictionary *, NSError *))callback {
     
 }
 
 - (void) fetchImageWithCallback: (void(^)(UIImage *, NSError *))callback {
+    NSString * dataUrl = @"https://httpbin.org/image/png";
+    NSURL * url = [NSURL URLWithString: dataUrl];
+    NSURLRequest * request = [NSURLRequest requestWithURL: url];
+    NSURLSession * session = [NSURLSession sharedSession];
     
+    NSURLSessionDataTask * dataTask =
+    [session dataTaskWithRequest: request
+               completionHandler: ^(NSData * _Nullable data,
+                                    NSURLResponse * _Nullable response,
+                                    NSError * _Nullable error) {
+                   
+                   dispatch_async(dispatch_get_main_queue(), ^{
+                       
+                       if (data != NULL) {
+                           UIImage * image = [UIImage imageWithData: data];
+                           callback(image, nil);
+                           
+                       } else {
+                           callback(nil, error);
+                       }
+                   });
+               }
+     ];
+    
+    [dataTask resume];
 }
 
 @end
--- WebServiceSDK/WebServiceSDK/ViewController.m
+++ WebServiceSDK/WebServiceSDK/ViewController.m
@@ -20,13 +20,71 @@
     self.httpClient = [[HTTPClient alloc] init];
     
     [self fetchData];
+    [self fetchImage];
+}
+
+- (void) addImageView: (UIImage *)image {
+    
+    UIImageView * pigView = [UIImageView new];
+    pigView.translatesAutoresizingMaskIntoConstraints = NO;
+    pigView.backgroundColor = [UIColor blueColor];
+    pigView.image = image;
+    [self.view addSubview: pigView];
+    
+    [pigView addConstraint: [NSLayoutConstraint constraintWithItem: pigView
+                                                         attribute: NSLayoutAttributeHeight
+                                                         relatedBy: NSLayoutRelationEqual
+                                                            toItem: nil
+                                                         attribute: 0
+                                                        multiplier: 1
+                                                          constant: 100]
+     ];
+    
+    [pigView addConstraint: [NSLayoutConstraint constraintWithItem: pigView
+                                                         attribute:NSLayoutAttributeWidth
+                                                         relatedBy: NSLayoutRelationEqual
+                                                            toItem: nil
+                                                         attribute: 0
+                                                        multiplier: 1
+                                                          constant: 100]
+     ];
+    
+    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: pigView
+                                                           attribute: NSLayoutAttributeCenterY
+                                                           relatedBy: NSLayoutRelationEqual
+                                                              toItem: self.view
+                                                           attribute:NSLayoutAttributeCenterY
+                                                          multiplier: 0.5
+                                                            constant: 0]
+     ];
+    
+    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: pigView
+                                                           attribute:NSLayoutAttributeCenterX
+                                                           relatedBy: NSLayoutRelationEqual
+                                                              toItem: self.view
+                                                           attribute: NSLayoutAttributeCenterX
+                                                          multiplier: 1 constant: 0]
+     ];
 }
 
 - (void) fetchData {
-    
-    [self.httpClient fetchGetResponseWithCallback:^(NSDictionary * dict, NSError * error) {
+    [self.httpClient fetchGetResponseWithCallback: ^(NSDictionary * dict, NSError * error) {
         if (error == NULL) {
             NSLog(@"%@", dict);
+        
+        } else {
+            NSLog(@"%@",error.localizedDescription);
+        }
+    }];
+}
+
+- (void) fetchImage {
+    [self.httpClient fetchImageWithCallback: ^(UIImage * image, NSError * error) {
+        if (error == NULL) {
+            NSLog(@"%@", image);
+            //            self->pigView.image = image;
+            [self addImageView: image];
+            
         } else {
             NSLog(@"%@",error.localizedDescription);
         }
