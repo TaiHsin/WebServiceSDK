5b62686208adf04443a4e0c627592065e769fad1
taihsinlee <taihsinlee@kkbox.com>
2019-02-12 12:43:21 +0800

    Implemented HTTPClient unit test

--- WebServiceSDK/WebServiceSDKTests/WebServiceSDKTests.m
+++ WebServiceSDK/WebServiceSDKTests/WebServiceSDKTests.m
@@ -7,6 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "HTTPClient.h"
+
+@interface HTTPClient (ForTesting)
+
+- (void)getData: (NSString *)dataUrl callback: (void(^)(NSData *, NSError *))callback;
+
+@end
 
 @interface WebServiceSDKTests : XCTestCase
 
@@ -14,24 +21,101 @@
 
 @implementation WebServiceSDKTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testGetDataSucceed {
+    
+    HTTPClient * httpClient = [HTTPClient new];
+    XCTestExpectation * expecatation = [self expectationWithDescription: @"Query failed!"];
+    NSString * dataUrl = @"https://httpbin.org/get";
+    
+    [httpClient getData: dataUrl callback: ^(NSData * data, NSError * error) {
+        
+        if (data != nil) {
+            [expecatation fulfill];
+        }
     }];
+    
+    [self waitForExpectationsWithTimeout: 2 handler: nil];
+}
+
+- (void)testGetDataFailed {
+    
+    HTTPClient * httpClient = [HTTPClient new];
+    XCTestExpectation * expecatation = [self expectationWithDescription: @"Query failed!"];
+    NSString * dataUrl = @"https://httpbin.org/gets";
+    
+    [httpClient getData: dataUrl callback: ^(NSData * data, NSError * error) {
+        
+        if (error != nil) {
+            [expecatation fulfill];
+        }
+    }];
+    
+    [self waitForExpectationsWithTimeout: 2 handler: nil];
+}
+
+- (void)testFetchGetResponseWithCallbackSucceed {
+    
+    HTTPClient * httpClient = [HTTPClient new];
+    XCTestExpectation * expecatation = [self expectationWithDescription: @"Query failed!"];
+    NSString * url = @"https://httpbin.org/get";
+    
+    __block NSString * urlToCheck = [NSString new];
+    [httpClient fetchGetResponseWithCallback: ^(NSDictionary * dict, NSError * error) {
+        
+        if (error == nil) {
+            urlToCheck = dict[@"url"];
+            [expecatation fulfill];
+            
+        } else {
+            XCTFail(@"Response data failed");
+        }
+    }];
+    
+    [self waitForExpectationsWithTimeout: 2 handler: nil];
+    XCTAssertTrue([url isEqualToString: urlToCheck]);
+}
+
+- (void)testFetchImageWithCallbackSucceed {
+    
+    HTTPClient * httpClient = [HTTPClient new];
+    XCTestExpectation * expecatation = [self expectationWithDescription: @"Query failed!"];
+    
+    __block UIImage * imageHolder = [UIImage new];
+    [httpClient fetchImageWithCallback: ^(UIImage * image, NSError * error) {
+        
+        if (error == nil) {
+            imageHolder = image;
+            [expecatation fulfill];
+            
+        } else {
+            XCTFail(@"Response data failed");
+        }
+    }];
+    
+    [self waitForExpectationsWithTimeout: 2 handler: nil];
+    XCTAssertTrue(imageHolder != nil);
+}
+
+- (void)testPostCustomerNameSucceed {
+    
+    HTTPClient * httpClient = [HTTPClient new];
+    XCTestExpectation * expecatation = [self expectationWithDescription: @"Query failed!"];
+    NSString * name = @"KKBOXTest";
+    
+    __block NSString * dictString = [NSString new];
+    [httpClient postCustomerName: @"KKBOXTest" callback: ^(NSDictionary * dict, NSError * error) {
+        
+        if (error == nil) {
+            dictString = dict[@"form"][@"custname"];
+            [expecatation fulfill];
+            
+        } else {
+            XCTFail(@"Response data failed");
+        }
+        
+    }];
+    [self waitForExpectationsWithTimeout: 2 handler: nil];
+    XCTAssertTrue([name isEqualToString: dictString]);
 }
 
 @end
